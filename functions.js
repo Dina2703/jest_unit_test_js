da9608d223331434c43083b126d87b7e40e2976f
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-05-11 14:40:00 -0400

    starter, first test, create add function and test it

---
+++ functions.js
@@ -0,0 +1,10 @@
+const functions = {
+  //with arrow function(short and clean syntax)
+  add: (num1, num2) => num1 + num2,
+  //regular function
+  add2: function (num1, num2) {
+    return num1 + num2;
+  },
+};
+
+module.exports = functions;
